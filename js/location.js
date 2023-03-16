599bfa50a1e5f69ce71eb3e6053d7f80a44c1b75
J3SUNG <wptjd6141@naver.com>
2023-03-17 02:23:05 +0900

    API test

--- js/location.js
+++ js/location.js
@@ -1,12 +1,25 @@
 // index page 로딩시 검색할 지역 정보 얻기.
 const serviceKey = `ubnkXnSW8ajYtdl2qGsp0c8z4d9%2B6njKihzCNrAq%2Fp2bpdu0yhN%2BtuDgdnjI9qcQBj9KkcbdY8%2F6ApS52nw%2F6g%3D%3D`;
-let areaCode;
-let detailAreaUrl = `https://apis.data.go.kr/B551011/KorService1/areaBasedSyncList1?${serviceKey}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&showflag=1&listYN=Y&arrange=A&contentTypeId=15&areaCode=${areaCode}`;
+let areaCode = "";
+let contentTypeId = 15;
+let detailAreaUrl = `https://apis.data.go.kr/B551011/KorService1/areaBasedSyncList1?serviceKey=${serviceKey}&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&showflag=1&listYN=Y&arrange=A&contentTypeId=${contentTypeId}&areaCode=${areaCode}`;
 let areaUrl = `https://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey=${serviceKey}&numOfRows=20&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json`;
+
+console.log(location.search);
+const params = new URLSearchParams(location.search);
+const loc = parseInt(params.get("loc"));
+const detailLoc = parseInt(params.get("detailLoc"));
+const search = parseInt(params.get("search"));
+// areaCode = loc > 0 ? loc : "";
+console.log(loc, detailLoc, search);
 
 fetch(areaUrl)
   .then((response) => response.json())
   .then((data) => makeArea(data));
+
+fetch(detailAreaUrl)
+  .then((response) => response.json())
+  .then((data) => receive(data));
 
 function makeArea(data) {
   let sel = document.querySelector("#search-area");
@@ -19,6 +32,6 @@ function makeArea(data) {
   });
 }
 
-function onClickSearch(data) {
-  console.log("A" + data);
+function receive(data) {
+  console.log(data);
 }
